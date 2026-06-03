a3cc13350a8392f60dac1950c9835f322812c686
Dion Verhoef <51044594+DionJCV@users.noreply.github.com>
2026-06-03 16:10:51 +0200

    ITransactionRepository methods added

--- backend/api/src/main/java/nl/donniebankoebarkie/api/repository/interfaces/ITransactionRepository.java
+++ backend/api/src/main/java/nl/donniebankoebarkie/api/repository/interfaces/ITransactionRepository.java
@@ -3,8 +3,11 @@ package nl.donniebankoebarkie.api.repository.interfaces;
 import nl.donniebankoebarkie.api.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface ITransactionRepository extends JpaRepository<Transaction, Long>{
-    Optional<Transaction> findById(Long transactionId);
+    List<Transaction> findByInitiated_by_user_id(Long id);
+    Transaction findTransactionById(Long id);
+
+    void createTransaction(Transaction transaction);
 }
